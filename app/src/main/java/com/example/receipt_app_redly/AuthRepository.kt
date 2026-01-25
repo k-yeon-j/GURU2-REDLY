bf509b7152e1e2a187d9ada5b6ed8fdf5b8252d0
k-yeon-j <k.yeonju0112@gmail.com>
2026-01-25 22:39:41 +0900

    Delete app directory

--- app/src/main/java/com/example/receipt_app_redly/AuthRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.receipt_app_redly
-
-interface AuthRepository {
-    fun login(id: String, password: String, callback: (Boolean) -> Unit)
-    fun signup(id: String, password: String, callback: (Boolean) -> Unit)
-}
--- app/src/main/java/com/example/receipt_app_redly/DataBridge.kt
+++
@@ -1,6 +0,0 @@
-package com.example.receipt_app_redly
-import android.graphics.Bitmap
-
-object DataBridge {
-    var tempBitmap: Bitmap? = null
-}
